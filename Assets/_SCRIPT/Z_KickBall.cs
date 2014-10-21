ea56f011c2e1b0d6fbd610669a5a9033c30c4301
Nicholas Zeman <nbzeman@gmail.com>
2014-10-21 11:56:59 -0400

    Set up Generic Joe Scene for Ragdoll and Mecanim Tutorial

---
+++ Assets/_SCRIPT/Z_KickBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_KickBall : MonoBehaviour {
+
+	public Camera maincam;
+	public int forcey;
+	public int forcez;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+
+	void OnMouseDown(){
+
+		gameObject.rigidbody.AddForce (0, forcey*1000, forcez*1000);
+	}
+}
---
+++ Assets/_SCRIPT/Z_RagdollKiller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_RagdollKiller : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = gameObject.GetComponentInParent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision thing){
+
+		if(thing.gameObject.tag =="ball"){
+			print (thing.gameObject.name+" hit me");
+			anim.enabled=false;
+		}
+	}
+
+	IEnumerator OnCollisionExit(Collision thing){
+
+		if(thing.gameObject.tag =="ball"){
+			yield return new WaitForSeconds(1);
+			anim.enabled=true;
+		}
+	}
+}
