016d8339ea5bf576543cc564bf5f8d08955668b0
nbzeman <nbzeman@gmail.com>
2014-10-22 20:24:46 -0400

    Finished tutorial for Mecanim/Ragdoll

---
+++ Assets/_SCRIPT/Z_AttackSimple.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_AttackSimple : MonoBehaviour {
+
+	public Animator anim;
+	AnimatorStateInfo stateInfo;
+	
+	//if we're in "Run" mode, respond to input for jump, and set the Jump parameter accordingly. 
+	//if(stateInfo.nameHash == Animator.StringToHash("Base Layer.RunBT"))
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+				if (Input.GetKeyDown ("p")) {
+						anim.SetTrigger ("Punch");
+				}
+				if (Input.GetKey ("g")) {
+						anim.SetBool ("Grab", true);
+				} else {
+						anim.SetBool ("Grab", false);
+				}
+		}
+}
